50682608a38a9dea4c8230521413fe2921828bd6
Antonio Pedreira <antoniohenriquep@outlook.com>
2022-12-30 12:50:41 -0300

    Adicionando o array de objetos

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import { View , Text , StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View , Text , StyleSheet, FlatList} from 'react-native';
 import Header from './src/Components/Header';
 
 export default function App() {
+
+  const [feed, setFeed] = useState([
+    {
+      id: '1',
+      nick:'antoniohp',
+      bio:'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+      profile:'https://m.media-amazon.com/images/M/MV5BMjQyNzM2MjM1Ml5BMl5BanBnXkFtZTcwMDE5NjI3Mg@@._V1_.jpg',
+      img:'https://www.geekbinge.com/wp-content/uploads/2014/12/Adam-Sandler-Sony.jpg',
+      likers:10,
+      liked: false
+    },
+    {
+      id: '2',
+      nick:'antoniohp',
+      bio:'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+      profile:'https://m.media-amazon.com/images/M/MV5BMjQyNzM2MjM1Ml5BMl5BanBnXkFtZTcwMDE5NjI3Mg@@._V1_.jpg',
+      img:'https://www.geekbinge.com/wp-content/uploads/2014/12/Adam-Sandler-Sony.jpg',
+      likers:104,
+      liked: true
+    },
+    {
+      id: '3',
+      nick:'antoniohp',
+      bio:'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+      profile:'https://m.media-amazon.com/images/M/MV5BMjQyNzM2MjM1Ml5BMl5BanBnXkFtZTcwMDE5NjI3Mg@@._V1_.jpg',
+      img:'https://www.geekbinge.com/wp-content/uploads/2014/12/Adam-Sandler-Sony.jpg',
+      likers:112,
+      liked: false
+    },
+    {
+      id: '4',
+      nick:'antoniohp',
+      bio:'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+      profile:'https://m.media-amazon.com/images/M/MV5BMjQyNzM2MjM1Ml5BMl5BanBnXkFtZTcwMDE5NjI3Mg@@._V1_.jpg',
+      img:'https://www.geekbinge.com/wp-content/uploads/2014/12/Adam-Sandler-Sony.jpg',
+      likers:300,
+      liked: false
+    },
+
+  ])
+
  return (
    <View>
     <Header/>
+    <FlatList
+    data={feed}
+    keyExtractor={(item) => item.id}
+    renderItem ={({item}) => <List data = {item}/>}
+    />
+
+
    </View>
   );
 }
---
+++ src/Components/List/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function List(props) {
+ return (
+   <View>
+    <Text>{data.props.name}</Text>
+    <Image/>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
