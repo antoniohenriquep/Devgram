ace0b565e235e8340b0e51fab3cebbaf3b73ef52
Antonio Pedreira <antoniohenriquep@outlook.com>
2022-12-29 23:03:41 -0300

    Concluindo Header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {View, StyleSheet, Text, Pressable} from 'react-native';
+import {View, StyleSheet, Text, Pressable, Image} from 'react-native';
 
 export default function Header() {
  return (
    <View style = {styles.header}>
-    <Pressable>
+    <Pressable style = {styles.logo}>
      <Text>Devgram</Text>
+    </Pressable>
+
+
+    <Pressable>
+      <Image 
+      source = {require('../../Images/Icons/icons8-send-24.png')}
+      style={styles.send}/>
     </Pressable>
    </View>
   );
@@ -14,6 +21,22 @@ export default function Header() {
 
 const styles = StyleSheet.create({
     header:{
-      flex:1
+      height: 55,
+      flexDirection:'row',
+      alignItems:'center',
+      justifyContent:'space-between',
+      paddingLeft: 15,
+      paddingRight: 15,
+      borderBottomWidth:0.2,
+      shadowColor:'#000',
+      elevation:1
+    },
+    logo:{
+      height:40,
+      width:90
+    },
+    send:{
+      width:24,
+      height:24
     }
   })
